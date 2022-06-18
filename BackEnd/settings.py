294480b1e94e82f21de6eae23d9c65d696d18ce7
ReF01L <salushkin1998@bk.ru>
2022-06-18 20:19:32 +1000

    Web 20

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
+__version__ = '0.0.2'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from vk_api import VkApi
 
